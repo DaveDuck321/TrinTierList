4c75dcacfe32f2a4318712fb2513e9cb1923d291
Thomas Grant <thomas.grant.duck321@gmail.com>
2020-01-23 13:02:55 +0000

    Improved api

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//Returns: {category:string, person1:{id:number, name:string, imgs:string[]}, ...}
+//Returns: {category:{id:int, name:string}, person1:{id:number, name:string, imgs:string[]}, ...}
 
 let CurrentRank = {
     id: [],
-    category: "",
+    category: 0,
 }
 
 function randomItem(list) {
@@ -12,20 +12,21 @@ function randomItem(list) {
 async function ShowPeople(category) {
     document.getElementById("category").innerHTML = "Loading...";
 
-    const result = await fetch(`?category=${category}`)
-    const data = await result.json;
+    const result = await fetch(`people?category=${category}`)
+    const data = await result.json();
 
-    document.getElementById("category").innerHTML = data.category;
+    console.log(data);
+    document.getElementById("category").innerHTML = data.category.name;
 
-    document.getElementById("person1-src").src = randomItem(data[0]);
-    document.getElementById("person2-src").src = randomItem(data[1]);
+    document.getElementById("person1-src").src = randomItem(data.person1.imgs);
+    document.getElementById("person2-src").src = randomItem(data.person2.imgs);
 
     document.getElementById("person1-name").innerHTML = data.person1.name;
     document.getElementById("person2-name").innerHTML = data.person2.name;
 
     CurrentRank = {
-        id: [data[0].id, data[1].id],
-        category: result.category,
+        id: [data.person1.id, data.person2.id],
+        category: result.category.id,
     };
 }
 
