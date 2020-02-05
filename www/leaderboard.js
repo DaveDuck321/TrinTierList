9e1e4f3af2b9cfebcf5aed1d90be37a52ab001fc
Thomas Grant <thomas.grant.duck321@gmail.com>
2020-02-05 18:49:02 +0000

    Populated leaderboard

--- www/leaderboard.js
+++ www/leaderboard.js
@@ -16,13 +16,34 @@ async function PostJSON(url, data) {
     return response;
 }
 
+function sortByCategory(people, elos, category) {
+    for(let person of people) {
+        person.elo = elos[category][person.id];
+    }
 
-async function PopulateLeaderboard() {
-    let { people, categories, elos } = await PostJSON("/api/leaderboard", {});
-    for (ranking of elos) {
-        let { id, elos } = ranking;
+    people.sort((p1, p2) => p2.elo - p1.elo );
+}
 
+async function PopulateLeaderboard(people, elos, categories, category) {
+    sortByCategory(people, elos, category);
+    document.getElementById("category").innerText = categories[category].name;
+
+    const leaderboard = document.getElementById("leaderboard");
+    const template = leaderboard.querySelector("template");
+    const row = template.content.querySelector("tr");
+
+    for (let [index, person] of people.entries()) {
+        let personRow = document.importNode(row, true);
+
+        personRow.querySelector(".rank").innerText = index + 1;
+        personRow.querySelector(".nickname").innerText = person.nickname;
+        personRow.querySelector(".elo").innerText = person.elo;
+
+        leaderboard.appendChild(personRow);
     }
 }
 
-document.addEventListener("DOMContentLoaded", PopulateLeaderboard);
+document.addEventListener("DOMContentLoaded", async ()=> {
+    let { people, categories, elos } = await PostJSON("/api/leaderboard", {});
+    PopulateLeaderboard(people, elos, categories, categories[0].id);
+});
